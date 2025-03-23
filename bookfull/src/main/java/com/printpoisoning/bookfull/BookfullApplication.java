ae1069e0b0bd1193c41bd20d66815cb60fb09397
jihyun.seo <jihyun.seo.dev@gmail.com>
2025-03-23 14:19:56 +0900

    Created: dev 브랜치 생성, test api 생성

--- bookfull/src/main/java/com/printpoisoning/bookfull/BookfullApplication.java
+++ bookfull/src/main/java/com/printpoisoning/bookfull/BookfullApplication.java
@@ -3,13 +3,7 @@ package com.printpoisoning.bookfull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;  
-
 @SpringBootApplication  
-@OpenAPIDefinition(info = @Info(title = "BookFull API Documentation",  
-                                description = "북풀 애플리케이션의 API 문서입니다.",  
-                                version = "1.0.0"))  
 public class BookfullApplication {
 
 	public static void main(String[] args) {
