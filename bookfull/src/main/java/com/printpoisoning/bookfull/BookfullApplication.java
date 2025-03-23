b7e671a3aa1021c9c85c3498f312de52cad8ac84
jihyun.seo <jihyun.seo.dev@gmail.com>
2025-03-23 16:43:27 +0900

    feat: Add initial Test API

---
+++ bookfull/src/main/java/com/printpoisoning/bookfull/BookfullApplication.java
@@ -0,0 +1,13 @@
+package com.printpoisoning.bookfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookfullApplication.class, args);
+	}
+
+}
