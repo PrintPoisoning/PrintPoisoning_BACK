3e4b5353f457bf1c56b172df5edccea211599c6d
jihyun.seo <jihyun.seo.dev@gmail.com>
2025-04-03 22:34:02 +0900

    fix: Swagger 페이지를 기본 주소로 접근할 수 있도록 설정

--- bookfull/src/main/java/com/printpoisoning/bookfull/config/OpenAPIConfiguration.java
+++ bookfull/src/main/java/com/printpoisoning/bookfull/config/OpenAPIConfiguration.java
@@ -1,17 +1,22 @@
 package com.printpoisoning.bookfull.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;  
+import io.swagger.v3.oas.models.info.Info;  
+import org.springframework.context.annotation.Bean;  
+import org.springframework.context.annotation.Configuration;  
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;  
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;  
 
 @Configuration
-public class OpenAPIConfiguration {
+public class OpenAPIConfiguration implements WebMvcConfigurer {
 
     private static final String API_NAME = "Book Full Application";
     private static final String API_VERSION = "1.0.0"; 
     private static final String API_DESCRIPTION = "책풀 애플리케이션입니다.";
     
+    public OpenAPIConfiguration() {  
+    }  
+
     @Bean
     public OpenAPI OpenAPIConfig(){
         return new OpenAPI()
@@ -20,4 +25,9 @@ public class OpenAPIConfiguration {
                         .description(API_DESCRIPTION)
                         .version(API_VERSION));
     }
+
+    @Override  
+    public void addViewControllers(ViewControllerRegistry registry) {  
+        registry.addRedirectViewController("/", "/swagger-ui/index.html#/");  
+    }  
 }
